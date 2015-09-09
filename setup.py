c824ed49fc8a403deb233b6582290aa360889994
Gustavo Abranches <gabranches@gmail.com>
2015-09-09 01:16:14 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='image-viewer',
+    version='0.1',
+    description='An image browser for Reddit.',
+    url='',
+    author='Gustavo Abranches',
+    author_email='gabranches@gmail.com',
+    packages=['Flask', 'requests']
+	)
