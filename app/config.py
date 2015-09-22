8d1cb1886f9634734ca11751936aab54795b35dd
Gustavo Abranches <gabranches@gmail.com>
2015-09-21 22:59:04 -0400

    URI change

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 CLIENT_ID = "mGEGfnX8-v9_TQ"
 CLIENT_SECRET = "LHRakhCrm1KC1GNFx07q7VBhQIw"
-REDIRECT_URI = "http://127.0.0.1:5000/mysubreddits"
+REDIRECT_URI = "http://vast-reef-3750.herokuapp.com/mysubreddits"
 SECRET_KEY = "asdfas9d87987asf09asdf78089as3,,3???"
