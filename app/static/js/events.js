5dc799708d43cb92d2523846aacf2ff37cb51b37
Gustavo Abranches <gabranches@gmail.com>
2015-10-14 21:56:19 -0400

    style fixes

--- app/static/js/events.js
+++ app/static/js/events.js
@@ -1,206 +1,206 @@
 // ----  Event Handlers ---- //
 
 // Clear input on focus
-$('#input-sub').focus(function() {		
-	$(this).attr('placeholder', '');
+$('#input-sub').focus(function() {
+    $(this).attr('placeholder', '');
 });
 
 // Add subreddit on enter
 $('#input-sub').keypress(function (e) {
-	if (e.which == 13) {
-		addSub($('#input-sub').val());
-	}
+    if (e.which == 13) {
+        addSub($('#input-sub').val());
+    }
 });
 
 // Add gallery name
 $(document).on('focusout', '#share-name', function() {
-	addGalleryName();
+    addGalleryName();
 });
 
 $('#share-name').keypress(function (e) {
-	if (e.which == 13) {
-		addGalleryName();
-	}
+    if (e.which == 13) {
+        addGalleryName();
+    }
 });
 
 // Show/Hide active subs list
 $(document).on('click', '#active-subs-label', function() {
-	if (state.active_subs == 1) {
-		$('#active-subs-arrow').attr('class', 'glyphicon glyphicon-triangle-right');
-		$('#sub-names').hide();
-		state.active_subs = 0;
-	} else {
-		$('#active-subs-arrow').attr('class', 'glyphicon glyphicon-triangle-bottom');
-		$('#sub-names').show();
-		state.active_subs = 1;
-	}
+    if (state.active_subs == 1) {
+        $('#active-subs-arrow').attr('class', 'glyphicon glyphicon-triangle-right');
+        $('#sub-names').hide();
+        state.active_subs = 0;
+    } else {
+        $('#active-subs-arrow').attr('class', 'glyphicon glyphicon-triangle-bottom');
+        $('#sub-names').show();
+        state.active_subs = 1;
+    }
 });
 
 // Show/Hide popular subs list
 $(document).on('click', '#pop-subs-label', function() {
-	if (state.pop_subs == 1) {
-		$('#pop-subs-arrow').attr('class', 'glyphicon glyphicon-triangle-right');
-		$('#pop-sub-names').hide();
-		state.pop_subs = 0;
-	} else {
-		$('#pop-subs-arrow').attr('class', 'glyphicon glyphicon-triangle-bottom');
-		$('#pop-sub-names').show();
-		state.pop_subs = 1;
-	}
+    if (state.pop_subs == 1) {
+        $('#pop-subs-arrow').attr('class', 'glyphicon glyphicon-triangle-right');
+        $('#pop-sub-names').hide();
+        state.pop_subs = 0;
+    } else {
+        $('#pop-subs-arrow').attr('class', 'glyphicon glyphicon-triangle-bottom');
+        $('#pop-sub-names').show();
+        state.pop_subs = 1;
+    }
 });
 
 
 // Remove all subs
 $(document).on('click', '#remove-all', function() {
-	removeAll();
+    removeAll();
 });
 
 // Show update button on change
 $('#share-name').focus(function() {
-	$('#update-button').show();
+    $('#update-button').show();
 });
 
 // Mobile top menu
 $('#options-enter').click(function() {
-	$('#left-menu').attr('class', 'col-xs-12');
-	$('#options-enter').hide();
-	$('#options-leave').show();
+    $('#left-menu').attr('class', 'col-xs-12');
+    $('#options-enter').hide();
+    $('#options-leave').show();
 });
 
 $('#options-leave').click(function() {
-	$('#left-menu').attr('class', 'hidden-xs hidden-sm col-md-2 affix-top');
-	$('#options-enter').show();
-	$('#options-leave').hide();
+    $('#left-menu').attr('class', 'hidden-xs hidden-sm col-md-2 affix-top');
+    $('#options-enter').show();
+    $('#options-leave').hide();
 });
 
 // Goto sub
 $(document).on('click', '.sub-link', function() {
-	var sublink = $(this).parent().attr('sub-id');
-	window.location.href = '/r/' + sublink;	
+    var sublink = $(this).parent().attr('sub-id');
+    window.location.href = '/r/' + sublink;	
 });
 
 // Edit gallery
 $(document).on('click', '#edit-gallery', function() {
-	editGallery();
+    editGallery();
 });
 
 // Options show text
 $('#options-text').click(function() {
-	if($(this).is(':checked')) {
-		options.show_text = true;
-	} else {
-		options.show_text = false;
-	}
-	refreshSub();
-	createOptionsCookie();
+    if($(this).is(':checked')) {
+        options.show_text = true;
+    } else {
+        options.show_text = false;
+    }
+    refreshSub();
+    createOptionsCookie();
 });
 
 // Options show NSFW
 $('#options-nsfw').click(function() {
-	if($(this).is(':checked')) {
-		options.show_nsfw = true;
-	} else {
-		options.show_nsfw = false;
-	}
-	refreshSub();
-	createOptionsCookie();
+    if($(this).is(':checked')) {
+        options.show_nsfw = true;
+    } else {
+        options.show_nsfw = false;
+    }
+    refreshSub();
+    createOptionsCookie();
 });
 
 // Remove sub
 $(document).on('click', '.remove-button', function() {
-	if (state.events == 1) {
-		removeSub($(this).attr('sub-id'));
-		$(this).parent().remove();
-	}
+    if (state.events == 1) {
+        removeSub($(this).attr('sub-id'));
+        $(this).parent().remove();
+    }
 });
 
 // Add sub from popular list
 $(document).on('click', '.add-button', function() {
-	if (state.events == 1) {
-		addSub($(this).attr('sub-id'));
-	}
+    if (state.events == 1) {
+        addSub($(this).attr('sub-id'));
+    }
 });
 
 // Autocomplete
 $('#input-sub').typeahead({
-	onSelect: function(item) {
+    onSelect: function(item) {
         addSub(item.value);
     },
     ajax: '/static/js/suggestions.js'
 });
            
 // Show Remove button
-$(document).on('mouseenter', '.sub', function() {  
-	$(this).find('.remove-button').show();
+$(document).on('mouseenter', '.sub', function() {
+    $(this).find('.remove-button').show();
 });
 
 // Hide Remove button
-$(document).on('mouseleave', '.sub', function() { 	
-	$(this).find('.remove-button').hide();
+$(document).on('mouseleave', '.sub', function() {
+    $(this).find('.remove-button').hide();
 });
 
 // Show add button
-$(document).on('mouseenter', '.pop-sub', function() {  
-	$(this).find('.add-button').show();
+$(document).on('mouseenter', '.pop-sub', function() {
+    $(this).find('.add-button').show();
 });
 
 // Hide add button
-$(document).on('mouseleave', '.pop-sub', function() { 	
-	$(this).find('.add-button').hide();
+$(document).on('mouseleave', '.pop-sub', function() {
+    $(this).find('.add-button').hide();
 });
 
 // Dropdown submit
-$('#select-sort').change(function() { 	
-	options.sort = $('#select-sort').find(':selected').val();
-	refreshSub();
+$('#select-sort').change(function() {
+    options.sort = $('#select-sort').find(':selected').val();
+    refreshSub();
 });
 
 // Pic info box show
-$(document).on('mouseenter', '.picbox', function() {  
-	$(this).find('.infobox').show();
+$(document).on('mouseenter', '.picbox', function() {
+    $(this).find('.infobox').show();
 });
 
 // Pic info box hide
-$(document).on('mouseleave', '.picbox', function() { 	
-	$(this).find(".infobox").hide();
+$(document).on('mouseleave', '.picbox', function() {
+    $(this).find(".infobox").hide();
 });
 
 // Auto-loader
 $('#gallery').on('scroll', function() {
     if($(this).scrollTop() + $(this).innerHeight() >= (getMaxColumnHeight() * .60)) {
         if (state.events == 1){
-			getItems();
-		}
+            getItems();
+        }
     }
 });
 
 // Load more button (in case auto-loader doesn't work)
-$(document).on('click', '.load-more', function() {	
-	if (state.events == 1) {
-		getItems();
-	}
+$(document).on('click', '.load-more', function() {
+    if (state.events == 1) {
+        getItems();
+    }
 });
 
 // Share
 $(document).on('click', '#share', function() {
-	$('.share-load').show();
-	$.ajax({
-		url: '/ajax/share?link=' + subs.join(','),
-		success: function(data){
-			state.key = data;
-			$('.share-load').hide();
-			$('.share-result').show();
-			$('.share-button-wrapper').hide();
-			$('#share-link').val('vuiit.com/g/' + data);				
-		}
-	});
+    $('.share-load').show();
+    $.ajax({
+        url: '/ajax/share?link=' + subs.join(','),
+        success: function(data){
+            state.key = data;
+            $('.share-load').hide();
+            $('.share-result').show();
+            $('.share-button-wrapper').hide();
+            $('#share-link').val('vuiit.com/g/' + data);
+        }
+    });
 });
 
 // Make picbox div a link
-$(document).on('click', '.picbox', function() { 		
-	window.open = $(this).find('#extlink').attr('href'); 
+$(document).on('click', '.picbox', function() {
+    window.open = $(this).find('#extlink').attr('href'); 
 });
 
 $(window).resize(function() {
-	resize();
+    resize();
 });
--- app/static/js/functions.js
+++ app/static/js/functions.js
@@ -1,252 +1,252 @@
 // ----  Functions ---- //
 
 function resize() {
-	var height = $(window).height();
-	$('#left-menu').css('height', height + 'px');
-	$('#gallery').css('height', height + 'px');
+    var height = $(window).height();
+    $('#left-menu').css('height', height + 'px');
+    $('#gallery').css('height', height + 'px');
 }
 
 function refreshSub() {
-	$('.load-button-wrapper').hide();
-	state.count = 0;
-	state.after = null;
-	clearCols();
-	getItems(subs.join('+'));
+    $('.load-button-wrapper').hide();
+    state.count = 0;
+    state.after = null;
+    clearCols();
+    getItems(subs.join('+'));
 }
 
 function clearCols() {
-	$('#col-1').empty();
-	$('#col-2').empty();
-	$('#col-3').empty();
-	$('#col-4').empty();
+    $('#col-1').empty();
+    $('#col-2').empty();
+    $('#col-3').empty();
+    $('#col-4').empty();
 }
 
 function addSub(sub) {
-	sub = sub.toLowerCase();
-	if (state.events == 1){
-		if (subs.indexOf(sub) == -1){
-			subs.push(sub);
-			addToSubList(sub);
-			createCookie('subsCookie', subs.join(','));
-			refreshSub();
-			$('.share-button-wrapper').show();
-			$('.share-result').hide();
-			$('#front-page-label').hide();
-		}
-		$('#input-sub').val('');
-		$('#input-sub').attr('placeholder','Add a Subreddit');
-		$('.dropdown-menu').hide();
-	}
+    sub = sub.toLowerCase();
+    if (state.events == 1){
+        if (subs.indexOf(sub) == -1){
+            subs.push(sub);
+            addToSubList(sub);
+            createCookie('subsCookie', subs.join(','));
+            refreshSub();
+            $('.share-button-wrapper').show();
+            $('.share-result').hide();
+            $('#front-page-label').hide();
+        }
+        $('#input-sub').val('');
+        $('#input-sub').attr('placeholder','Add a Subreddit');
+        $('.dropdown-menu').hide();
+    }
 }
 
 function addGalleryName() {
-	var name = $('#share-name').val();
-	$('#update-button').hide();
-	$('#update-loader').show();
-	$.ajax({
-		url: '/ajax/updatename?name=' + name + '&key=' + state.key,
-		success: function(data){
-			$('#update-loader').hide();
-			$('#update-confirm').show().delay(3000).fadeOut();;
-		}
-	});
+    var name = $('#share-name').val();
+    $('#update-button').hide();
+    $('#update-loader').show();
+    $.ajax({
+        url: '/ajax/updatename?name=' + name + '&key=' + state.key,
+        success: function(data){
+            $('#update-loader').hide();
+            $('#update-confirm').show().delay(3000).fadeOut();;
+        }
+    });
 }
 
 function removeAll() {
-	subs = [];
-	state.sub = '';
-	eraseCookie('subsCookie');
-	$('.share-button-wrapper').hide();
-	$('.share-result').hide();
-	$('#sub-names').empty();
-	$('#sub-names').html('<span id="front-page-label">Front Page</span>');
-	$('#front-page-label').show();
-	refreshSub();
+    subs = [];
+    state.sub = '';
+    eraseCookie('subsCookie');
+    $('.share-button-wrapper').hide();
+    $('.share-result').hide();
+    $('#sub-names').empty();
+    $('#sub-names').html('<span id="front-page-label">Front Page</span>');
+    $('#front-page-label').show();
+    refreshSub();
 }
 
 function editGallery() {
-	createCookie('subsCookie', subs.join(','));
-	window.location.href = '/';
+    createCookie('subsCookie', subs.join(','));
+    window.location.href = '/';
 }
 
 function removeSub(sub) {
-	var index = subs.indexOf(sub);
-	subs.splice(index,1);
-	createCookie('subsCookie', subs.join(','));
-	if (subs.length == 0) {
-		eraseCookie('subsCookie');
-		$('#front-page-label').show();
-		$('.share-button-wrapper').hide();
-	} else {
-		$('.share-button-wrapper').show();
-	}
-	$('.share-result').hide();
-	refreshSub();
+    var index = subs.indexOf(sub);
+    subs.splice(index,1);
+    createCookie('subsCookie', subs.join(','));
+    if (subs.length == 0) {
+        eraseCookie('subsCookie');
+        $('#front-page-label').show();
+        $('.share-button-wrapper').hide();
+    } else {
+        $('.share-button-wrapper').show();
+    }
+    $('.share-result').hide();
+    refreshSub();
 }
 
 function addToSubList(sub) {
-	sub = sub.toLowerCase();
-	if (state.page == 'main') {
-		$('#sub-names').append('<div sub-id="' +
-			sub + '" class="row sub"><span class="sub-link">r/' +
-			sub + '</span><span class="remove-button glyphicon glyphicon-minus" sub-id="' +
-			sub + '" title="remove this subreddit"></span></div>');
-	} else {
-		$('#sub-names').append('<div sub-id="' +
-			sub + '" class="row sub"><span class="sub-link">r/' +
-			sub + '</span></div>');
-	}
-	$('.share-button-wrapper').show();
+    sub = sub.toLowerCase();
+    if (state.page == 'main') {
+        $('#sub-names').append('<div sub-id="' +
+            sub + '" class="row sub"><span class="sub-link">r/' +
+            sub + '</span><span class="remove-button glyphicon glyphicon-minus" sub-id="' +
+            sub + '" title="remove this subreddit"></span></div>');
+    } else {
+        $('#sub-names').append('<div sub-id="' +
+            sub + '" class="row sub"><span class="sub-link">r/' +
+            sub + '</span></div>');
+    }
+    $('.share-button-wrapper').show();
 }
 
 function addToPopularList(sub) {
-	$('#pop-sub-names').append('<div sub-id="' +
-		sub + '" class="row pop-sub"><span class="sub-link">r/' +
-		sub + '</span><span class="add-button glyphicon glyphicon-plus" sub-id="' +
-		sub + '" title="add this subreddit"></span></div>');
+    $('#pop-sub-names').append('<div sub-id="' +
+        sub + '" class="row pop-sub"><span class="sub-link">r/' +
+        sub + '</span><span class="add-button glyphicon glyphicon-plus" sub-id="' +
+        sub + '" title="add this subreddit"></span></div>');
 }
 
 function getPopularSubs() {
-	$.getJSON("https://www.reddit.com/subreddits/popular/.json?limit=100", function(data) {
-		$.each(data.data.children,function(index,element) { 
-			addToPopularList(element.data.display_name.toLowerCase());
-		});
-	});
+    $.getJSON("https://www.reddit.com/subreddits/popular/.json?limit=100", function(data) {
+        $.each(data.data.children,function(index,element) { 
+            addToPopularList(element.data.display_name.toLowerCase());
+        });
+    });
 }
 
 function getItems() {
 
-	var sub = subs.join('+');
-	state.events = 0;
-	var subUrl = (sub == '' ) ? '' : 'r/' + sub;
-	var limitUrl = 'limit=' + options.limit;
-	var afterUrl = (state.after == null) ? '' : '&after=' + state.after;
-	var countUrl = (state.count == 0) ? '' : '&count=' + state.count;
+    var sub = subs.join('+');
+    state.events = 0;
+    var subUrl = (sub == '' ) ? '' : 'r/' + sub;
+    var limitUrl = 'limit=' + options.limit;
+    var afterUrl = (state.after == null) ? '' : '&after=' + state.after;
+    var countUrl = (state.count == 0) ? '' : '&count=' + state.count;
 
-	switch(options.sort) {
-		case 'hot':
-			var sortType = 'hot';
-			var sortUrl = 'sort=hot';
-			break;
-		case 'new':
-			var sortType = 'new';
-			var sortUrl = 'sort=new';
-			break;
-		case 'day':
-			var sortType = 'top';
-			var sortUrl = 'sort=top&t=day';
-			break;
-		case 'week':
-			var sortType = 'top';
-			var sortUrl = 'sort=top&t=week';
-			break;
-		case 'month':
-			var sortType = 'top';
-			var sortUrl = 'sort=top&t=month';
-			break;
-		case 'year':
-			var sortType = 'top';
-			var sortUrl = 'sort=top&t=year';
-			break;
-		case 'all':
-			var sortType = 'top';
-			var sortUrl = 'sort=top&t=all';
-			break;
-	}
-	
-	var url = 'http://www.reddit.com/' + subUrl + '/' + sortType + '/.json?' + sortUrl + '&' + limitUrl + afterUrl + countUrl;
+    switch(options.sort) {
+        case 'hot':
+            var sortType = 'hot';
+            var sortUrl = 'sort=hot';
+            break;
+        case 'new':
+            var sortType = 'new';
+            var sortUrl = 'sort=new';
+            break;
+        case 'day':
+            var sortType = 'top';
+            var sortUrl = 'sort=top&t=day';
+            break;
+        case 'week':
+            var sortType = 'top';
+            var sortUrl = 'sort=top&t=week';
+            break;
+        case 'month':
+            var sortType = 'top';
+            var sortUrl = 'sort=top&t=month';
+            break;
+        case 'year':
+            var sortType = 'top';
+            var sortUrl = 'sort=top&t=year';
+            break;
+        case 'all':
+            var sortType = 'top';
+            var sortUrl = 'sort=top&t=all';
+            break;
+    }
+    
+    var url = 'http://www.reddit.com/' + subUrl + '/' + sortType + '/.json?' + sortUrl + '&' + limitUrl + afterUrl + countUrl;
 
-	$.getJSON( url, function(data) {
-		state.events = 1;
-		state.url = url;
-		listItems(data);
-		if (state.last_page == true) {
-			$('.load-button-wrapper').hide();
-		} else {
-			$('.load-button-wrapper').show();
-		}
-	}).fail(function() {
-		window.alert('Could not fetch data. Please check that the subreddits are valid.');
-		state.events = 1;
-	});
+    $.getJSON( url, function(data) {
+        state.events = 1;
+        state.url = url;
+        listItems(data);
+        if (state.last_page == true) {
+            $('.load-button-wrapper').hide();
+        } else {
+            $('.load-button-wrapper').show();
+        }
+    }).fail(function() {
+        window.alert('Could not fetch data. Please check that the subreddits are valid.');
+        state.events = 1;
+    });
 }
 
 function setColumnHeights() {
-	for (var i=1; i<5; i++) {
-		state['columns'][i]['height'] = 0;
-	}
-	for (var i=1; i<5; i++) {
-		$('#col-' + i + '-wrapper').children().each(function(){
-			state['columns'][i]['height'] += $(this).innerHeight();
-		})
-	}
+    for (var i=1; i<5; i++) {
+        state['columns'][i]['height'] = 0;
+    }
+    for (var i=1; i<5; i++) {
+        $('#col-' + i + '-wrapper').children().each(function(){
+            state['columns'][i]['height'] += $(this).innerHeight();
+        })
+    }
 }
 
 function getMaxColumnHeight() {
-	setColumnHeights();
-	return Math.max(
-		state['columns']['1']['height'],
-		state['columns']['2']['height'],
-		state['columns']['3']['height'],
-		state['columns']['4']['height'] 
-	);
+    setColumnHeights();
+    return Math.max(
+        state['columns']['1']['height'],
+        state['columns']['2']['height'],
+        state['columns']['3']['height'],
+        state['columns']['4']['height'] 
+    );
 }
 
 function loadCookies() {
-	optionsCookie = JSON.parse(readCookie('options'));
-	// Show text posts
-	if (optionsCookie.show_text != false) {
-		options.show_text = optionsCookie.show_text;
-		$('#options-text').prop('checked', true);
-	}
-	// Show NSFW posts
-	if (optionsCookie.show_nsfw != false) {
-		options.show_nsfw = optionsCookie.show_nsfw;
-		$('#options-nsfw').prop('checked', true);
-	}
+    optionsCookie = JSON.parse(readCookie('options'));
+    // Show text posts
+    if (optionsCookie.show_text != false) {
+        options.show_text = optionsCookie.show_text;
+        $('#options-text').prop('checked', true);
+    }
+    // Show NSFW posts
+    if (optionsCookie.show_nsfw != false) {
+        options.show_nsfw = optionsCookie.show_nsfw;
+        $('#options-nsfw').prop('checked', true);
+    }
 }
 
 function createOptionsCookie() {
-	createCookie('options', JSON.stringify(options));
+    createCookie('options', JSON.stringify(options));
 }
 
 function listItems(json) {
-	// Compile handlebars template
-	var raw_template = $('#simple-template').html();
-	var pic_template = Handlebars.compile(raw_template);
-	var raw_template = $('#info-template').html();
-	var info_template = Handlebars.compile(raw_template);
+    // Compile handlebars template
+    var raw_template = $('#simple-template').html();
+    var pic_template = Handlebars.compile(raw_template);
+    var raw_template = $('#info-template').html();
+    var info_template = Handlebars.compile(raw_template);
 
-	if (json.data.after == null) {
-		state.last_page = true;
-	}
+    if (json.data.after == null) {
+        state.last_page = true;
+    }
  
-	$.each(json.data.children, function(i, element) { // Iterate through JSON object
-		if(element.data.thumbnail == 'nsfw') {
-			element.data.thumbnail = '/static/images/nsfw.png';
-			if (options.show_nsfw == false) {
-				return true;
-			}
-		}
-		// Determine in which column to place the next picture
-		var cur_col = (state.count % 4) + 1;
-		var placeHolder = $('#col-' + cur_col);
-		element.data.col = 'col-' + cur_col;
+    $.each(json.data.children, function(i, element) { // Iterate through JSON object
+        if(element.data.thumbnail == 'nsfw') {
+            element.data.thumbnail = '/static/images/nsfw.png';
+            if (options.show_nsfw == false) {
+                return true;
+            }
+        }
+        // Determine in which column to place the next picture
+        var cur_col = (state.count % 4) + 1;
+        var placeHolder = $('#col-' + cur_col);
+        element.data.col = 'col-' + cur_col;
 
-		if(element.data.thumbnail == 'self' || element.data.thumbnail == 'default' || element.data.thumbnail == ''){
-			if(options.show_text == true) {
-				if (element.data.stickied == false) {
-					var html = info_template(element); // Generate the HTML for each post
-					placeHolder.append(html); // Render the posts into the page
-				}
-			}
-		} else {
-			if (element.data.stickied == false) {
-				var html = pic_template(element); // Generate the HTML for each post
-				placeHolder.append(html); // Render the posts into the page
-			}
-		}
-		state.after = element.data.name;
-		state.count++;
-	});
+        if(element.data.thumbnail == 'self' || element.data.thumbnail == 'default' || element.data.thumbnail == ''){
+            if(options.show_text == true) {
+                if (element.data.stickied == false) {
+                    var html = info_template(element); // Generate the HTML for each post
+                    placeHolder.append(html); // Render the posts into the page
+                }
+            }
+        } else {
+            if (element.data.stickied == false) {
+                var html = pic_template(element); // Generate the HTML for each post
+                placeHolder.append(html); // Render the posts into the page
+            }
+        }
+        state.after = element.data.name;
+        state.count++;
+    });
 }
